2df724161ec7b18fc786fd38ec14db085344c781
asse2024 <159319454+asse2024@users.noreply.github.com>
2024-02-09 14:05:43 +0300

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,21 +1,28 @@
-from flask import Flask,request,abort, app
+import hmac
+from flask import Flask, request
 
 app = Flask(__name__)
-@app.route('/webhook', methods =['POST'] )
-def webhook():
-    if request.method == 'POST':
-        print (request.json);
+
+
+@app.route('/receive-seatable-webhook', methods=['POST'])
+def receive():
+
+    secret = 'secret'
+    seatable_signature = request.headers.get('X-Seatable-Signature', '').replace('sha256=', '')
+
+    signature = hmac.new(
+        secret.encode('utf-8'), request.data, digestmod='sha256').hexdigest()
+
+    signature_compare = hmac.compare_digest(signature, seatable_signature)
+
+    if signature_compare:
+        print(request.json)
         return 'success', 200
-    else:
-           abort(400)
-           
-
-if __name__:'__main__'
-app.run()
-
-
-
-    
+        # do something
+  
+    return {
+    'success':signature_compare
+    }
 
 
 
